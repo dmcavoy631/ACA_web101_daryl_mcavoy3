7c9c7dd2bd5da9584947dd71fdab8e69a386eb40
Daryl McAvoy <dmcavoy631@gmail.com>
2019-12-17 21:53:07 -0600

    more progres

--- src/JavaScript/cp3-stressReliever.js
+++ src/JavaScript/cp3-stressReliever.js
@@ -1,18 +1,64 @@
+$(document).ready(function() {
 
-var video = document.getElementById("myVideo");
+
+
+var $count = 0;
+var $x = null;
+
+// Add animation shake and remove at animationend
+  $('.icon').click(function(){
+    $(this).addClass('animated shake').one('animationend',function(){
+      $(this).removeClass('animated shake');});
+    $x = $(this).clone();
+
+    $('.box').append($x);
+    $('.spaceCats').append($x);
     
-// Get the button
-var btn = document.getElementById("myBtn");
+  // increase count and console
+  $count++;
+  console.log($count);
+
+
+  // Add animation rubberBand to .next() and remove at animationend
+    $(this).next().addClass('animated rubberBand').one('animationend',function(){
+      $(this).removeClass('animated rubberBand');});
+    // This will add a counter but needs work.
+    // ,$(this).html($count)
+
+
+
+
+
+    // add count this = next()
+    // console.log(this);
+
     
+
+    // Alert Box
+    if($count > 20) {
+      alert("This is getting werid...maybe you should stop")
+    } 
+
+  });
+
+
+
+var video = $('#myVideo').get(0);
+// // Get the button
+var btn = $("#myBtn");
+
 // Pause and play the video, and change the button text
-function myFunction() {
-  if (video.paused) {
-      video.play();
-      btn.innerHTML = "Pause";
-    } else {
-      video.pause();
-      btn.innerHTML = "Play";
-      }
-    }
 
-$()
+  $('#myBtn').click(function(){
+    if (video.paused) {
+        video.play();
+        $(this).html('Pause');
+      } else {
+        video.pause();
+        $(this).html('Play');
+        }
+  });
+
+
+
+});
